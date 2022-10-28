6f26a82b4a2b9a2fe3375f8840588ebbd4503e08
Ahsaask <ahsaaskaushal@yahoo.com>
2022-10-27 19:16:45 -0600

    Revert "Update App.tsx"
    This reverts commit 5fbecccbc2fe732fe48ba774c2f877f2f793a7b9.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <ul>
           <li>Ben</li>
-          <li>Ahsaas</li>
         </ul>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
