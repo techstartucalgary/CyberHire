a676e6e0001a18b5f2f6c8a81fda2fe516239f64
Ben Schmidt <ben@schmidtfam.us>
2022-10-27 19:06:02 -0600

    Created List

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ul>
+          <li>Ben</li>
+        </ul>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
