c32f9f633411fecdfe9b9ab6fd5906ac60cfc59b
anagarzaa <analaura.espinosagar@ucalgary.ca>
2022-10-27 19:15:13 -0600

    Update App.tsx
    Signed-off-by: Ana Garza <analaura.espinosagar@ucalgary.ca>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <ul>
           <li>Ben</li>
+          <li>Ana</li>
         </ul>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
