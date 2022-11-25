8c52ad6f5398111b4fdf9db97e49962bc7cfe17d
Ben Schmidt <ben@schmidtfam.us>
2022-11-24 19:47:04 -0700

    Installed react-router-dom, started Router setup (works locally)
    Signed-off-by: Ben Schmidt <ben@schmidtfam.us>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      {/* TODO: Figure out why exact doesn't work */}
+      <Route path="" element={<App />} />
+      <Route path="/about" element={<App />} />
+    </Routes>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
