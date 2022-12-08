976af329c12cf7b820a718157f1f09988345a494
longfight123 <lingphoonlee@gmail.com>
2022-12-07 18:19:15 -0700

    ddl for the users table in postgres

--- backend/users_ddl.sql
+++ backend/users_ddl.sql
@@ -1,1 +1,10 @@
-SE
+CREATE TABLE IF NOT EXISTS cyberhire."user" (
+	id serial4 NOT NULL,
+	username varchar(100) NOT NULL,
+	"password" varchar(100) NOT NULL,
+	email varchar(50) NOT NULL,
+	isrecruiter bool NOT NULL,
+	CONSTRAINT user_email_unique_2 UNIQUE (email),
+	CONSTRAINT user_pkey PRIMARY KEY (id),
+	CONSTRAINT user_username_unique_1 UNIQUE (username)
+)
