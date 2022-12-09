5c7f145c3fdbf67a8a110bae3c1a63462b690e0d
Bobinho100 <114113147+Bobinho100@users.noreply.github.com>
2022-12-08 18:51:33 -0700

    new changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,24 @@
 import React from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          <li>Ben</li>
-          <li>Ahsaas</li>
-          <li>Etta</li>
-          <li>Ana</li>
-          <li>Ling</li>
-        </ul>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <form>
+
+        <p className = "title">Login Page</p>
+        <form className = "App">
+
+          <input type = "text" />
+          <input type = "email" />
+          <input t
+        </form>
     </div>
-  );
+    
+    
+ );
 }
 
 export default App;
