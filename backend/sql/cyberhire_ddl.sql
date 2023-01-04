dbcc490ff828769a4c86374fafa083ddfdf11cb7
longfight123 <lingphoonlee@gmail.com>
2023-01-04 11:55:13 -0700

    change isrecruiter to is_recruiter to match pydantic models
    Signed-off-by: longfight123 <lingphoonlee@gmail.com>

--- backend/sql/cyberhire_ddl.sql
+++ backend/sql/cyberhire_ddl.sql
@@ -3,22 +3,22 @@ CREATE TABLE IF NOT EXISTS cyberhire."user" (
 	username varchar(100) NOT NULL,
 	"password" varchar(100) NOT NULL,
 	email varchar(50) NOT NULL,
-	isrecruiter bool NOT NULL,
+	is_recruiter bool NOT NULL,
 	CONSTRAINT user_pkey PRIMARY KEY (id),
 	CONSTRAINT user_email_unique_2 UNIQUE (email),
 	CONSTRAINT user_username_unique_1 UNIQUE (username)
-)
+);
 
 CREATE TABLE IF NOT EXISTS cyberhire."skill" (
 	id serial4 NOT NULL,
 	skill varchar(30) NOT NULL UNIQUE,
 	CONSTRAINT skill_pkey PRIMARY KEY (id),
 	CONSTRAINT skill_skill_unique UNIQUE (skill)
-)
+);
 
 CREATE TABLE IF NOT EXISTS cyberhire.user_skill (
 	user_id int4 NOT NULL,
 	skill_id int4 NOT NULL,
-	CONSTRAINT user_skill_user_fk FOREIGN KEY user_id REFERENCES cyberhire.user (id),
+	CONSTRAINT user_skill_user_fk FOREIGN KEY (user_id) REFERENCES cyberhire.user (id),
 	CONSTRAINT user_skill_skill_fk2 FOREIGN KEY (skill_id) REFERENCES cyberhire.skill (id)
-)
+);
