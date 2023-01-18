94df6973befec7a6f71416e517ac670bcb9ae8fb
longfight123 <lingphoonlee@gmail.com>
2023-01-18 07:57:03 -0700

    Define GET route for a individual user's profile.
    Route defined is /useres/profile/{username}
    
    Signed-off-by: longfight123 <lingphoonlee@gmail.com>

--- backend/routers/user_profile_router.py
+++ backend/routers/user_profile_router.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Path
 from sqlalchemy.orm import Session
 from ..schemas import user_profile_schema
 from ..models import user_profile_model
 from ..database import SessionLocal
 from .. import dependencies
-from ..crud import user_profile_crud
+from ..crud import user_profile_crud, user_crud
 
 router = APIRouter()
 
@@ -31,7 +31,7 @@ def get_user_profiles(db: Session = Depends(dependencies.get_db)):
     summary="Get the current user's profile information.",
     description="Get the current logged in user's profile information, including " \
         "first name, last name, profile picture as bytes string, resume " \
-        "as bytes string, and a list of skills",
+        "as bytes string, and a list of skills.",
     response_description="The current user's profile information."
 )
 def get_user_profile_me(
@@ -40,3 +40,22 @@ def get_user_profile_me(
 ):
     return user_profile_crud.get_user_profile_by_id(db, current_user.id)
 
+@router.get(
+    "/users/profile/{username}",
+    response_model=user_profile_schema.UserProfile,
+    status_code=status.HTTP_200_OK,
+    tags=["UserProfile"],
+    summary="Get a user's profile information.",
+    description="Get a current user's profile information, including " \
+        "first name, last name, profile picture as bytes string, resume " \
+        "as bytes string, and a list of skills.",
+    response_description="A user's profile information."
+)
+def get_user_profile(
+    db: Session = Depends(dependencies.get_db),
+    username: str = Path()
+):
+    user = user_crud.get_user_by_username(db, username)
+    if user is None:
+        raise HTTPException(status_code=400, detail="Username does not exist.")
+    return user_profile_crud.get_user_profile_by_id(db, user.id)
