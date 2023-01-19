3c1fd308ad92af5c203f7d2587d58515ce7b7209
Ben Schmidt <ben@schmidtfam.us>
2023-01-19 15:48:02 -0700

    Updated Suhaib's last name, fixed errors

--- frontend/src/Pages/About.tsx
+++ frontend/src/Pages/About.tsx
@@ -1,71 +1,70 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "../logo.svg";
+import "../App.css";
 
 function About() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <head>
-        <title>About Our Company</title>
-        <link>
-        </link>
-      </head>
-      <body>
-        <main>
-          <h2>Our Story</h2>
-          <p> CyberHire was kickstarted by a group of 6 multidisciplinary individuals from the University of Calgary Tech Start club for university students.
-              Our team's vision originated from sharing the pain of the job seeking experience, which motivated us to make the process more intuitive
-              and stress free for everyone else. We'll be starting out with the focus of marketing towards tech students due to one of our main competitive advantages
-              being primarily a group of tech concentration students. However we do have a focus of expanding into other concentrations and niches of the job market
-              once our website has seen an significant amount of impact within our initial target market.
-          </p>
-          <h2>Our Team</h2>
-          <div className ="team-member">
-            <img src="/public/team-member-1.jpg" alt="Team Member 1">
-              <h3>Ben Schmidt</h3>
-              <p>Project Manager</p>
-            </img>
-          </div>
-          <div className ="team-member">
-            <img src="/public/team-member-2.jpg" alt="Team Member 2">
-              <h3>Ana Garza </h3>
-              <p> Junior Frontend Developer</p>
-            </img>
-          </div>
-          <div className ="team-member">
-            <img src="/public/team-member-3.jpg" alt="Team Member 3">
-              <h3>Suhaib "Last Name" </h3>
-              <p>Developer</p>
-            </img>
-          </div>
-          <div className ="team-member">
-            <img src="/public/team-member-4.jpg" alt="Team Member 4">
-              <h3>Ling Lee</h3>
-              <p>Fullstack Developer</p>
-            </img>
-          </div>
-          <div className ="team-member">
-            <img src="/public/team-member-5.jpg" alt="Team Member 4">
-              <h3>Bernard Aire</h3>
-              <p>Frontend Developer</p>
-            </img>
-          </div>
-          <div className ="team-member">
-            <img src="/public/team-member-6.jpg" alt="Team Member 4">
-              <h3>Etta Liu</h3>
-              <p>Business Strategist & Frontend Developer</p>
-            </img>
-          </div> 
-        </main>
-        <footer>
-          <p>Copyright 2022 CyberHire</p>
-        </footer>
-      </body>
-    </div>
-    );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+			</header>
+			<h1>
+				<title>About Our Company</title>
+				<link></link>
+			</h1>
+
+			<h2>Our Story</h2>
+			<p>
+				{" "}
+				CyberHire was kickstarted by a group of 6 multidisciplinary
+				individuals from the University of Calgary Tech Start club for
+				university students. Our team's vision originated from sharing
+				the pain of the job seeking experience, which motivated us to
+				make the process more intuitive and stress free for everyone
+				else. We'll be starting out with the focus of marketing towards
+				tech students due to one of our main competitive advantages
+				being primarily a group of tech concentration students. However
+				we do have a focus of expanding into other concentrations and
+				niches of the job market once our website has seen an
+				significant amount of impact within our initial target market.
+			</p>
+			<h2>Our Team</h2>
+			<div className="team-member">
+				<img src="/public/team-member-1.jpg" alt="Team Member 1"></img>
+				<h3>Ben Schmidt</h3>
+				<p>Project Manager</p>
+			</div>
+			<div className="team-member">
+				<img src="/public/team-member-2.jpg" alt="Team Member 2"></img>
+				<h3>Ana Garza </h3>
+				<p> Junior Frontend Developer</p>
+			</div>
+			<div className="team-member">
+				<img src="/public/team-member-3.jpg" alt="Team Member 3"></img>
+				<h3>Suhaib Tariq </h3>
+				<p>Developer</p>
+			</div>
+			<div className="team-member">
+				<img src="/public/team-member-4.jpg" alt="Team Member 4"></img>
+				<h3>Ling Lee</h3>
+				<p>Fullstack Developer</p>
+			</div>
+			<div className="team-member">
+				<img src="/public/team-member-5.jpg" alt="Team Member 4"></img>
+				<h3>Bernard Aire</h3>
+				<p>Frontend Developer</p>
+			</div>
+			<div className="team-member">
+				<img src="/public/team-member-6.jpg" alt="Team Member 4"></img>
+				<h3>Etta Liu</h3>
+				<p>Business Strategist & Frontend Developer</p>
+			</div>
+
+			<footer>
+				<p>Copyright 2022 CyberHire</p>
+			</footer>
+		</div>
+	);
 }
 
 export default About;
