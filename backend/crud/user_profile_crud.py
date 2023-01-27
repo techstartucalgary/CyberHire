32dbb1d4f02f44695c28c8480f6619431dd00c52
longfight123 <lingphoonlee@gmail.com>
2023-01-26 19:06:09 -0700

    Add CRUD utilities to create user profile and update resume and profile picture.
    Signed-off-by: longfight123 <lingphoonlee@gmail.com>

--- backend/crud/user_profile_crud.py
+++ backend/crud/user_profile_crud.py
@@ -8,4 +8,49 @@ def get_user_profiles(db: Session):
 
 def get_user_profile_by_id(db: Session, user_id: int):
 
-    return db.query(user_profile_model.UserProfile).filter(user_id == user_profile_model.UserProfile.user_id).first()
+    return db.query(user_profile_model.UserProfile).filter(
+        user_id == user_profile_model.UserProfile.user_id).first()
+
+def create_user_profile(
+    db: Session,
+    user_id: int,
+    user_profile: user_profile_schema.UserProfileCreate
+):
+
+    db_user_profile = user_profile_model.UserProfile(
+        user_id=user_id,
+        **user_profile.dict()
+    )
+    db.add(db_user_profile)
+    db.commit()
+    db.refresh(db_user_profile)
+
+    return db_user_profile
+
+def update_user_resume(db: Session, user_id: int, resume: bytes | None) \
+    -> user_profile_model.UserProfile | None:
+
+    db_user_profile = db.query(user_profile_model.UserProfile) \
+        .filter(user_id == user_profile_model.UserProfile.user_id) \
+        .first()
+        
+    if db_user_profile:
+        db_user_profile.resume = resume
+        db.commit()
+        db.refresh(db_user_profile)
+
+        return db_user_profile
+
+def update_user_profile_picture(db: Session, user_id: int, profile_picture: bytes | None) \
+    -> user_profile_model.UserProfile | None:
+
+    db_user_profile = db.query(user_profile_model.UserProfile) \
+        .filter(user_profile_model.UserProfile.user_id == user_id) \
+        .first()
+
+    if db_user_profile:
+        db_user_profile.profile_picture = profile_picture
+        db.commit()
+        db.refresh(db_user_profile)
+
+    return db_user_profile
