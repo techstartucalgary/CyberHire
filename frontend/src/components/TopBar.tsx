433264499f71d52a597d809c839e0e482777ffc7
Ben Schmidt <ben@schmidtfam.us>
2023-03-02 18:54:02 -0700

    Finished topbar

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -1,29 +1,37 @@
 import React from "react";
-import {
-	AppBar,
-	Button,
-	Toolbar,
-	Typography,
-} from "@mui/material";
+import { AppBar, Button, Toolbar, Typography, Grid } from "@mui/material";
 import logo from "./CyberHireLogo.png";
 import "../styles/TopBar.css";
 
 function TopBar() {
-	return (
-		<AppBar position="sticky">
-			<Toolbar>
-				<a href="/">
-					<img className="logo" src={logo} alt="CyberHire Logo" />
-				</a>
-				<Button variant="contained">
-					<Typography>Sign-up</Typography>
-				</Button>
-				<Button variant="outlined">
-					<Typography>Sign in</Typography>
-				</Button>
-			</Toolbar>
-		</AppBar>
-	);
+  return (
+    <AppBar position="sticky">
+      <Toolbar className="topBar">
+        <Grid container spacing={0} columns={3} alignItems="center">
+          <Grid item xs={0} md={1} />
+          <Grid
+            className="logoContainer"
+            item
+            xs={2}
+            md={1}
+            alignItems="center"
+          >
+            <a href="/" className="logoLink">
+              <img className="logo" src={logo} alt="CyberHire Logo" />
+            </a>
+          </Grid>
+          <Grid item className="buttonContainer" xs={1}>
+            <Button variant="contained" className="signup">
+              <Typography>Sign-up</Typography>
+            </Button>
+            <Button variant="outlined" className="signin">
+              <Typography>Sign in</Typography>
+            </Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default TopBar;
