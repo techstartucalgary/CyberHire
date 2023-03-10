5963b7b99122ec03ebf255d3c5f368e867942d53
longfight123 <lingphoonlee@gmail.com>
2023-03-09 18:18:33 -0700

    define route and crud operation to update a job
    Signed-off-by: longfight123 <lingphoonlee@gmail.com>

--- backend/crud/job_crud.py
+++ backend/crud/job_crud.py
@@ -2,19 +2,19 @@ from sqlalchemy.orm import Session
 from ..models import jobs_model
 from ..schemas import job_schema
 
-def get_all_jobs(db: Session):
+def get_all_jobs(db: Session) -> list[jobs_model.Job]:
 
     return db.query(jobs_model.Job).all()
 
-def get_job_by_id(db: Session, job_id: int):
+def get_job_by_id(db: Session, job_id: int) -> jobs_model.Job | None:
 
     return db.query(jobs_model.Job).filter(jobs_model.Job.id == job_id).first()
 
-def get_jobs_by_user_profile_id(db: Session, user_profile_id: int):
+def get_jobs_by_user_profile_id(db: Session, user_profile_id: int) -> list[jobs_model.Job]:
 
     return db.query(jobs_model.Job).filter(jobs_model.Job.user_profile_id == user_profile_id).all()
 
-def create_job(db: Session, user_profile_id: int, new_job: job_schema.JobCreate):
+def create_job(db: Session, user_profile_id: int, new_job: job_schema.JobCreate) -> jobs_model.Job:
 
     # Convert pydantic schema to a sql alchemy model
     new_job_model = jobs_model.Job(user_profile_id=user_profile_id, **new_job.dict())
@@ -24,3 +24,17 @@ def create_job(db: Session, user_profile_id: int, new_job: job_schema.JobCreate)
     db.refresh(new_job_model)
 
     return new_job_model
+
+def update_job(db: Session, new_job: job_schema.JobInDb) -> jobs_model.Job | None:
+
+    current_job = db.query(jobs_model.Job).filter(jobs_model.Job.id == new_job.id).first()
+
+    if current_job is not None:
+        current_job.title = new_job.title
+        current_job.description = new_job.description
+        current_job.location = new_job.location
+        current_job.min_salary = new_job.min_salary
+        current_job.max_salary = new_job.max_salary
+    db.commit()
+    db.refresh(current_job)
+    return current_job
--- backend/routers/job_router.py
+++ backend/routers/job_router.py
@@ -80,12 +80,12 @@ def get_all_recruiter_jobs(db: Session=Depends(dependencies.get_db),
         "Salary is optional if not provided.",
     response_description="The newly created job posting in the database."
 )
-def create_job(*, db: Session = Depends(dependencies.get_db),
+def create_job(*, db: Session=Depends(dependencies.get_db),
                recruiter: user_model.User=Depends(dependencies.get_current_recruiter_user),
                new_job: job_schema.JobCreate):
     
     # Check if user profile exists
-    recruiter_profile = dependencies.user_profile_exists(db, recruiter.id)
+    dependencies.user_profile_exists(db, recruiter.id)
 
     # Create the job
     new_job_db = job_crud.create_job(db, recruiter.id, new_job)
@@ -93,7 +93,57 @@ def create_job(*, db: Session = Depends(dependencies.get_db),
     return new_job_db
 
 # PATCH /jobs/{job_id} update a job
+@router.patch(
+    "/jobs/{job_id}",
+    response_model=job_schema.Job,
+    status_code=status.HTTP_200_OK,
+    tags=["Job"],
+    summary="PATCH route to update an existing recruiter's posted job.",
+    description="PATCH route to update a job posted by the current recruiter. " \
+        "Recruiters can only update their own jobs. All information is optional, " \
+        "if a value is not provided for a field, the current value of the field " \
+        "in the database remains unchanged.",
+    response_description="The job with the updated information."
+)
+def update_job(*, db: Session=Depends(dependencies.get_db),
+               recruiter: user_model.User=Depends(dependencies.get_current_recruiter_user),
+               new_job: job_schema.JobPatch,
+               job_id: int = Path()):
+    
+    # Check if the job exists and the recruiter posted the job
+    job_in_db_model = job_crud.get_job_by_id(db, job_id)
+
+    if job_in_db_model is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Job with id: {job_id} does not exist.")
+    
+    if job_in_db_model.user_profile_id != recruiter.id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail=f"Recruiter with id: {recruiter.id} is not authorized to " \
+                                "update this job.")
+
+    # Convert the job from the database to a pydantic model
+    job_in_db_schema = job_schema.JobInDb(
+        title=job_in_db_model.title,
+        description=job_in_db_model.description,
+        location=job_in_db_model.location,
+        min_salary=job_in_db_model.min_salary,
+        max_salary=job_in_db_model.max_salary,
+        id=job_in_db_model.id,
+        user_profile_id=job_in_db_model.user_profile_id
+    )
+
+    # Update the pydantic model with the new job information
+    job_in_db_schema.copy(update=new_job.dict())
+
+    # Save the new job information to the database
+    updated_job = job_crud.update_job(db, job_in_db_schema)
+
+    # Return the new job information
+    return updated_job
 
 # DELETE /jobs/{job_id} delete a job
+
+
 
 # POST /jobs/{job_id}/skills 
