036899cacf55c402d7774ca77191038e3f454ad8
longfight123 <lingphoonlee@gmail.com>
2023-03-14 09:09:27 -0600

    add documentation
    Signed-off-by: longfight123 <lingphoonlee@gmail.com>

--- backend/crud/job_crud.py
+++ backend/crud/job_crud.py
@@ -3,18 +3,77 @@ from ..models import jobs_model
 from ..schemas import job_schema
 
 def get_all_jobs(db: Session) -> list[jobs_model.Job]:
+    """
+    Utility function to return all jobs in the database
+
+    Parameters
+    ----------
+    db: Session
+        a database session
+
+    Returns
+    -------
+    list[jobs_model.Job]
+        list of sqlalchemy Job objects
+    """
 
     return db.query(jobs_model.Job).all()
 
 def get_job_by_id(db: Session, job_id: int) -> jobs_model.Job | None:
+    """
+    Utility function to return a job by job id
+
+    Parameters
+    ----------
+    db: Session
+        a database sesion
+    job_id:
+        the job's id in the database
+
+    Returns
+    -------
+    jobs_model.Job | None
+        a sqlalchemy Job object, or None if no job with the id is found
+    """
 
     return db.query(jobs_model.Job).filter(jobs_model.Job.id == job_id).first()
 
 def get_jobs_by_user_profile_id(db: Session, user_profile_id: int) -> list[jobs_model.Job]:
+    """
+    Utility function to get all jobs posted by a recruiter
+
+    Parameters
+    ----------
+    db: Session
+        a database session
+    user_profile_id: int
+        the recruiters id
+    
+    Returns
+    -------
+    list[jobs_model.Job]
+        a list of sqlalchemy Job objects that were posted by the recruiter
+    """
 
     return db.query(jobs_model.Job).filter(jobs_model.Job.user_profile_id == user_profile_id).all()
 
 def create_job(db: Session, user_profile_id: int, new_job: job_schema.JobCreate) -> jobs_model.Job:
+    """
+    Utility function to create a new job for the recruiter with id = user_profile_id
+
+    Parameters
+    ----------
+    db: Session
+        a database session
+    user_profile_id: int
+        the recruiters id
+    new_job: job_schema.JobCreate
+        a pydantic model for creating a new job
+
+    Returns
+    jobs_model.Job
+        a sqlalchemy Job object representing the newly created Job
+    """
 
     # Convert pydantic schema to a sql alchemy model
     new_job_model = jobs_model.Job(user_profile_id=user_profile_id, **new_job.dict())
@@ -26,6 +85,21 @@ def create_job(db: Session, user_profile_id: int, new_job: job_schema.JobCreate)
     return new_job_model
 
 def update_job(db: Session, new_job: job_schema.JobInDb) -> jobs_model.Job | None:
+    """
+    Utility function to update a job in the database
+
+    Parameters
+    ----------
+    db: Session
+        a database session
+    new_job: job_schema.JobInDb
+        a pydantic model with the updated job information
+
+    Returns
+    -------
+    jobs_model.Job | None
+        a sqlalchemy Job object with the updated information, or None if no job is found
+    """
 
     current_job = db.query(jobs_model.Job).filter(jobs_model.Job.id == new_job.id).first()
 
@@ -40,6 +114,21 @@ def update_job(db: Session, new_job: job_schema.JobInDb) -> jobs_model.Job | Non
     return current_job
 
 def delete_job(db: Session, job_id: int) -> jobs_model.Job | None:
+    """
+    Utility function to delete a job with job id = job_id from the database
+
+    Parameters
+    ----------
+    db: Session
+        a database session
+    job_id: int
+        the job's id in the database
+    
+    Returns
+    -------
+    jobs_model.Job | None
+        a sqlalchemy object representing the job that was deleted, or None if no job was found
+    """
 
     job_in_db = get_job_by_id(db, job_id)
     job_in_db_copy = None
