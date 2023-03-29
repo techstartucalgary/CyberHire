a2fe4d078adb211560addbb0e9300fbb87411763
longfight123 <lingphoonlee@gmail.com>
2023-03-29 08:40:42 -0600

    add job matching router to main
    Signed-off-by: longfight123 <lingphoonlee@gmail.com>

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routers import metadata_router, user_router, user_profile_router, \
-                     user_profile_skill_router, job_router, job_skill_router
+                     user_profile_skill_router, job_router, job_skill_router, \
+                     job_matching_router
 
 app = FastAPI()
 
@@ -25,3 +26,4 @@ app.include_router(user_profile_skill_router.router)
 app.include_router(metadata_router.router)
 app.include_router(job_router.router)
 app.include_router(job_skill_router.router)
+app.include_router(job_matching_router.router)
