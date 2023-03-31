26540d44a7bba323c520beb555203381545a9b18
longfight123 <lingphoonlee@gmail.com>
2023-03-30 19:03:03 -0600

    fix job matching algorithm to filter jobs applicant has already applied to
    Signed-off-by: longfight123 <lingphoonlee@gmail.com>

--- backend/crud/job_matching_crud.py
+++ backend/crud/job_matching_crud.py
@@ -1,25 +1,31 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import func, desc
 
-from ..models import jobs_model, job_skill_model, user_profile_model, user_profile_skill_model
+from ..models import jobs_model, job_skill_model, user_profile_model, \
+                     user_profile_skill_model, user_profile_job_model
 
 def get_job_matching(db: Session, user_id: int) -> list[jobs_model.Job]:
 
     # define subquery to match user_profile to user_profile_skills
-    q_inner = db.query(user_profile_model.UserProfile, user_profile_skill_model.UserProfileSkill)\
+    q_inner_user = db.query(user_profile_model.UserProfile, user_profile_skill_model.UserProfileSkill)\
         .join(user_profile_skill_model.UserProfileSkill,
               user_profile_skill_model.UserProfileSkill.user_profile_id == user_profile_model.UserProfile.user_id)\
         .filter(user_profile_model.UserProfile.user_id == user_id).subquery(name="C")
 
+    # define subquery to find list of job id's applicant already applied to
+    q_inner_applied_jobs = db.query(user_profile_job_model.UserProfileJob.job_id)\
+        .filter(user_profile_job_model.UserProfileJob.user_profile_id == user_id)
+
     # define outerquery to join job to job_skills, then the result joined to the subquery
     q_outer = db.query(jobs_model.Job.id, func.count(jobs_model.Job.id))\
         .join(job_skill_model.JobSkill, job_skill_model.JobSkill.job_id == jobs_model.Job.id)\
-        .join(q_inner, q_inner.c.skill_id == job_skill_model.JobSkill.skill_id)\
+        .join(q_inner_user, q_inner_user.c.skill_id == job_skill_model.JobSkill.skill_id)\
+        .filter(~jobs_model.Job.id.in_(q_inner_applied_jobs))\
         .group_by(jobs_model.Job.id)\
         .order_by(func.count(jobs_model.Job.id).desc())\
         .limit(10)
 
-    # print(str(q_outer))
+    print(str(q_outer))
     # print(q_outer.all())
 
     # get a list of job_ids
