0d56623cb8f7390c87285547aa42d617c2cf68b8
longfight123 <lingphoonlee@gmail.com>
2023-04-03 09:12:27 -0600

    add documentation
    Signed-off-by: longfight123 <lingphoonlee@gmail.com>

--- backend/schemas/application_status_schema.py
+++ backend/schemas/application_status_schema.py
@@ -1,10 +1,16 @@
 from pydantic import BaseModel, Field
 
 class ApplicationStatusBase(BaseModel):
+    """
+    Base model for a job's application status.
+    """
 
     status: str = Field(max_length=30)
 
 class ApplicationStatus(ApplicationStatusBase):
+    """
+    Model for returning a job's application status.
+    """
 
     id: int
 
