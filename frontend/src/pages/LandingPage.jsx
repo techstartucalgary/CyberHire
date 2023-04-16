a6201a0779ad7425b505753add5954faca3f897e
Ben Schmidt <ben@schmidtfam.us>
2023-04-16 10:50:18 -0600

    Minor reformatting of landing page

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Box, Button } from "@mui/material";
+import HiringVector from "../img/HiringVector.png";
+import vector1 from "../img/LPVector1.png";
+import vector2 from "../img/LPVector2.png";
+import vector3 from "../img/LPVector3.png";
+import findJob from "../img/findJob.png";
+import "../styles/LandingPage.css";
+
+const LandingPage = () => {
+	return (
+		<div>
+			<img src={vector1} />
+
+			<Box className="findJob">
+				<img src={findJob} />
+				<Button
+					className="button-1"
+					variant="contained"
+					sx={{ color: "black", backgroundColor: "#f9e393" }}
+					href="#/signup"
+				>
+					Get Started
+				</Button>
+			</Box>
+
+			<img src={HiringVector} />
+
+			<div className="design">
+				<img className="image" src={vector2} />
+				<img className="image" src={vector3} />
+			</div>
+		</div>
+	);
+};
+
+export default LandingPage;
