7b533268b00856167fde07ce21f7ad56aab8fe11
Ahsaask <109789003+Ahsaask@users.noreply.github.com>
2023-04-23 13:46:52 -0600

    Added basic view for Job listing -Recruiter

--- frontend/src/components/CreateJobModal.jsx
+++ frontend/src/components/CreateJobModal.jsx
@@ -15,50 +15,43 @@ import "../styles/Modal.css";
 function CreateJobModal(props) {
   const [showSalary, setShowSalary] = useState(false);
   const [salary, setSalary] = useState([0, 0]);
-  const [showGenericError, setShowGenericError] = useState(false);
+  // const [showGenericError, setShowGenericError] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+  const [company_name, setCompanyName] = useState(""); //  added Company Name in order to Get Job Posted by Recruiter
 
   const handleSalaryChange = (e, newValue) => {
     setSalary(newValue);
   };
 
-  const submitNewJob = async (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const min_salary = salary[0];
-    const max_salary = salary[1];
-    const title = formData.get("title");
-    const description = formData.get("description");
-    const location = formData.get("location");
-    const data = {
-      title,
-      description,
-      location,
-      min_salary,
-      max_salary,
-    };
-
-    await fetch("https://chapi.techstartucalgary.com/jobs", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          if (response.status === 401) {
-            window.location.href = "#/signin";
-          } else {
-            setShowGenericError(true);
-          }
-          throw new Error("Job creation failed");
-        }
-        props.closeModal();
-      })
-      .catch((error) => {
-        console.error(error);
+  const submitNewJob = async () => {
+    try {
+      const response = await fetch("https://chapi.techstartucalgary.com/jobs", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+        body: JSON.stringify({
+          title,
+          location,
+          description,
+          min_salary: salary[0],
+          max_salary: salary[1],
+          company_name, // added company name 
+        }),
       });
+
+      if (!response.ok) {
+        throw new Error("Job creation failed");
+      }
+
+      props.closeModal();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const formatCurrency = (num) => {
@@ -72,15 +65,32 @@ function CreateJobModal(props) {
   return (
     <Dialog open={props.open} fullWidth>
       <DialogTitle>Create New Job</DialogTitle>
-      <form className="form" onSubmit={submitNewJob}>
-        <TextField name="title" label="Job Title" required />
+      <form className="form" onSubmit={(e) => e.preventDefault()}>
+        <TextField
+          name="title"
+          label="Job Title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <TextField
           name="description"
           label="Job Description"
           multiline
           required
+          onChange={(e) => setDescription(e.target.value)}
         />
-        <TextField name="location" label="Location" required />
+        <TextField
+          name="location"
+          label="Location"
+          required
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <TextField
+          name="company_name"
+          label="Company Name"
+          required
+          onChange={(e) => setCompanyName(e.target.value)} 
+        />
         <FormControlLabel
           control={
             <Switch
@@ -106,19 +116,27 @@ function CreateJobModal(props) {
           </div>
         )}
         <div className="row right-align">
-          {showGenericError && (
+         (
             <Typography color="error">
               There was an error creating your job. Please try again.
             </Typography>
-          )}
+          )
         </div>
         <div className="row right-align button-container">
-          <Button variant="outlined" onClick={props.closeModal}>
+        <Button variant="outlined" onClick={(e) => {e.preventDefault(); props.closeModal();}}>
+
             Cancel
           </Button>
-          <Button type="submit" variant="contained">
-            Post Job
-          </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              onClick={submitNewJob}
+              disabled={!title || !description || !location || !company_name} // Disable the button if any of these fields are empty
+            >
+              Create Job
+            </Button>
+
         </div>
       </form>
     </Dialog>
