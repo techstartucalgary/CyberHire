a1eb94b9ce5ce2397123291bf9defa188659e7d2
Ben Schmidt <ben@schmidtfam.us>
2023-04-28 17:05:26 -0600

    Merge branch 'main' into RecruiterApplicants

---
+++ frontend/src/components/JobApply.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { Typography } from "@mui/material";
+import JobPost from "./JobPost";
+
+const JobApply = () => {
+  const [appliedJobs, setAppliedJobs] = useState([]);
+
+  const getAplliedJobs = async () => {
+    await fetch("https://chapi.techstartucalgary.com/applications/me", {
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setAppliedJobs(data))
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    getAplliedJobs();
+  }, []);
+
+  return (
+    <div>
+      {appliedJobs.length > 0 ? (
+        <div>
+          {appliedJobs.map((appliedJob) => (
+            <JobPost
+              key={appliedJob.id}
+              job={appliedJob.job}
+              disabled={true}
+              status={appliedJob.application_status.status}
+            />
+          ))}
+        </div>
+      ) : (
+        <Typography>No found Job</Typography>
+      )}
+    </div>
+  );
+};
+
+export default JobApply;
